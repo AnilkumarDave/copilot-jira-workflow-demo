fe6f3261cac9e13580c45f55289129650c0c9b5d
Anilkumar Dave <daveanil48@gmail.com>
2026-09-16 16:52:21 +0100

    Add initial Spring Boot application

---
+++ src/main/java/com/anilkumar/demo/CopilotJiraWorkflowDemoApplication.java
@@ -0,0 +1,13 @@
+package com.anilkumar.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotJiraWorkflowDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotJiraWorkflowDemoApplication.class, args);
+	}
+
+}
