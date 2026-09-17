c62a617e7aed74046a6ce708fd0c9676edd78fef
Anilkumar Dave <daveanil48@gmail.com>
2026-09-17 20:25:52 +0100

    Add Docker support and application health endpoint

--- src/main/java/com/anilkumar/demo/user/UserController.java
+++ src/main/java/com/anilkumar/demo/user/UserController.java
@@ -3,6 +3,7 @@ package com.anilkumar.demo.user;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Copilot Jira CI/CD Docker Application is running successfully!";
+    }
 
     @PostMapping
     public ResponseEntity<String> createUser(@Valid @RequestBody UserRequest request) {
